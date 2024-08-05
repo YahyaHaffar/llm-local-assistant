8936ca28664867ea6b4bc7ab9419c1f1564731b4
YahyaHaffar <129509469+YahyaHaffar@users.noreply.github.com>
2024-08-05 10:27:42 +0200

    Update llama3-assistant.py
    removing redundant query_prompt

--- llama3-assistant.py
+++ llama3-assistant.py
@@ -82,21 +82,6 @@ def load_prompts():
         """,
         input_variables=["question"],
     )
-
-    query_prompt = PromptTemplate(
-        template="""
-        <|begin_of_text|>
-        <|start_header_id|>system<|end_header_id|>
-        You are an expert at crafting web search queries for research questions.
-        More often than not, a user will ask a basic question that they wish to learn more about, however it might not be in the best format.
-        Reword their query to be the most effective web search string possible.
-        Return the JSON with a single key 'query' with no preamble or explanation.
-        Question to transform: {question}
-        <|eot_id|>
-        <|start_header_id|>assistant<|end_header_id|>
-        """,
-        input_variables=["question"],
-    )
     
     return generate_prompt, router_prompt, query_prompt
 
